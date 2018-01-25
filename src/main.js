9959fb1ca5a7dfe9b25b5eaec900896a7f1e23d4
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-01-25 14:46:21 +0800

    格式化

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Toasted from 'vue-toasted'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Toasted from "vue-toasted";
 
-import isPc from 'common/js/isPc'
-import 'common/stylus/index.styl'
-if(isPc() == 'phone') {
-  require.ensure([], () => {    
-     require('common/js/flexible')
-  })
-}else{
-  document.querySelector('html').style.fontSize = '108px'
+import isPc from "common/js/isPc";
+import "common/stylus/index.styl";
+if (isPc() == "phone") {
+  require.ensure([], () => {
+    require("common/js/flexible");
+  });
+} else {
+  document.querySelector("html").style.fontSize = "108px";
 }
 
-Vue.use(Toasted,{
-  position:'top-left',
-  duration:3000
-})
+Vue.use(Toasted, {
+  position: "top-left",
+  duration: 3000
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
